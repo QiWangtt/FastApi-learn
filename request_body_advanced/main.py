7c001bb4d458a0d86f4352f82ac334eb1ea98b31
QiWang <Qi.Wang@tracetronic.de>
2024-09-11 10:45:54 +0200

    request body

--- request_body_advanced/main.py
+++ request_body_advanced/main.py
@@ -15,8 +15,19 @@ app = FastAPI()
 
 
 class Address(BaseModel):
-    address: str
-    postcode: str
+    address: str = Field(..., examples=["5 Queens Street"]) # 1. 通过Field设定 example数据，最值得推荐的方法
+    postcode: str = Field(..., examples=["0765"])
+
+
+    '''2. 直接用model_config也可以设定示例数据'''
+    model_config = {
+        "json_schema_extra":{
+            "examples":[{
+                "adress": "2 Queens Street",
+                "Postcode": "0987"
+            }]
+        }
+    }
 
 
 # 对类里面的项目进行验证
@@ -36,8 +47,20 @@ class Item(BaseModel):
     features: List[Feature]
 
 
+# python基础，加了*号之后必须使用关键字传参， 示例
+# def add(*, num1: int, num2: int):
+#     return num1 + num2
+#
+# print(add(num1=1, num2=2))
+
+
 @app.put('/carts/{cart_id}')
-async def update_cart(cart_id: int, user: User, item: Item, count: int = Body(..., ge = 2)): # 加上一个单一参数count的验证，限制count大于等于2
+# 加上一个单一参数count的验证，限制count大于等于2
+# 3. 在body内部也可以设定示例参数
+# 三种方法优先级 model_config > Field > Body， 其中Field方法适合维护，如果不需要某参数，直接在Field中删除一次就行
+async def update_cart(cart_id: int, user: User, item: Item, count: int = Body(..., ge = 2, examples=[8])):
+# 在函数体内加上*号表示函数内的参数变成关键字参数，没有位置参数了，传参时必须用关键字传参
+# async def update_cart(*, cart_id: int, user: User = Body(..., examples=[{"Username": "Smith"}]), item: Item, count: int = Body(..., ge = 2, examples=[8])):
     print(user.username)
     print(item.name)
     result_dict ={
